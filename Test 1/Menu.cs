351c4619c280642ff7eea9594021e6dddfd65f3b
nilu92 <niklas.lund@hotmail.se>
2020-06-17 19:17:25 +0200

    lagt till en fiol

---
+++ Test 1/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Menu
+{
+	public void Menu()
+	{
+
+		Console.WriteLine("Det här är en meny");
+	
+	}
+}
